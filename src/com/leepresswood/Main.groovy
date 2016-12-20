21d1ddff96cd1f613cdd3d7a64acacb222588363
Lee Presswood <LeePresswood@gmail.com>
2016-12-19 18:36:16 -0600

    Wrote fitness finder.

--- src/com/leepresswood/Main.groovy
+++ src/com/leepresswood/Main.groovy
@@ -3,18 +3,23 @@ package com.leepresswood
 class Main {
     public static void main(String[] args) {
         //First, get text to match against.
-
+        Text text = getTextFromFile("resources/word.txt")
+        int textLength = text.text.size()
 
         //Next, create a population of randomly generated elements.
-
+        int populationSize = 100
+        Text[] population = []
+        populationSize.times { population << new Text(textLength)}
 
         //Determine if any of these elements match the matched text.
-
-
+        int attempts = 0
+        if(population.any { it.getFitnessScore(text) == textLength}){
+            println "Text match found after $attempts attempt(s)."
+        }
 
     }
 
-    private static Text getTextFromFile(String file){
-        
+    private static Text getTextFromFile(String fileName) {
+        new Text(new File(fileName).text)
     }
 }
--- src/com/leepresswood/Text.groovy
+++ src/com/leepresswood/Text.groovy
@@ -1,13 +1,25 @@
 package com.leepresswood
 
 class Text {
-    char[] text
+    String text
+
+    public Text(String text){
+        this.text = text
+    }
 
     public Text(int length){
 
     }
 
-    public int getFitnessScore(){
+    public int getFitnessScore(Text original){
+        int count = 0
+        for(int index = 0; index < original.text.size(); index++) {
+            //Increment the counter variable for any characters that are the same.
+            if(original.text.charAt(index) == this.text.charAt(index)){
+                count++
+            }
+        }
 
+        count
     }
 }
